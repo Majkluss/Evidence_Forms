bc84ada00cadddf5613eb12b34315aa950854390
Majkluss <Michaluss@seznam.cz>
2020-08-05 20:59:24 +0200

    Add project files.

---
+++ Evidence_Forms/App.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Evidence_Forms
+{
+    public partial class App : Application
+    {
+        const string prichod = "prichod";
+        const string odchod = "odchod";
+        const string saldo = "saldo";
+        const string saldoCelkem = "saldoCelkem";
+
+        public DateTime Prichod { get; set; }
+        public DateTime Odchod { get; set; }
+        public TimeSpan Saldo { get; set; }
+        public TimeSpan SaldoCelkem { get; set; }
+            public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            if(Properties.ContainsKey(prichod))
+            {
+                Prichod = (DateTime)Properties[prichod];
+            }
+
+            if(Properties.ContainsKey(odchod))
+            {
+                Odchod = (DateTime)Properties[odchod];
+            }
+
+            if(Properties.ContainsKey(saldo))
+            {
+                Saldo = (TimeSpan)Properties[saldo];
+            }
+
+            if (Properties.ContainsKey(saldoCelkem))
+            {
+                Saldo = (TimeSpan)Properties[saldoCelkem];
+            }
+        }
+
+        protected override void OnSleep()
+        {
+            Properties[prichod] = Prichod;
+            Properties[odchod] = Odchod;
+            Properties[saldo] = Saldo;
+            Properties[saldoCelkem] = SaldoCelkem;
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Evidence_Forms/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Xamarin.Forms;
+
+namespace Evidence_Forms
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            VypocitejSaldo();
+            // Nastaví texty s aktuálními časy
+            NastavTexty();
+
+            // Načte původní čas příchodu
+            prichodCas = (Application.Current as App).Prichod;
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        public TimeSpan delkaSmeny = new TimeSpan(0, 8, 0, 1, 0);
+        public DateTime prichodCas = new DateTime();
+        public DateTime odchodCas = new DateTime();
+        public TimeSpan saldo = new TimeSpan();
+        public TimeSpan saldoCelkem = new TimeSpan();
+
+        void PrichodBtn(object sender, EventArgs e)
+        {
+            prichodCas = DateTime.Now;
+            (Application.Current as App).Prichod = prichodCas;
+            Application.Current.SavePropertiesAsync();
+            NastavTextPrichod();
+        }
+
+        void OdchodBtn(object sender, EventArgs e)
+        {
+            odchodCas = DateTime.Now;
+            (Application.Current as App).Odchod = odchodCas;
+            VypocitejSaldo();
+            (Application.Current as App).Saldo = saldo;
+            Application.Current.SavePropertiesAsync();
+            NastavTexty();
+        }
+
+        void VypocitejSaldo()
+        {
+            saldo = (odchodCas - prichodCas)-delkaSmeny;
+            saldoCelkem += saldo;
+        }
+
+        void NastavTextPrichod()
+        {
+            prichodLabel.Text = ((Application.Current as App).Prichod).ToString("HH:mm:ss");
+        }
+
+        void NastavTextOdchod()
+        {
+            odchodLabel.Text = ((Application.Current as App).Odchod).ToString("HH:mm:ss");
+        }
+
+
+
+        void NastavTextSaldo()
+        {
+            string formatSaldo = (saldo < TimeSpan.Zero ? "\\-" : "") + "hh\\:mm\\:ss";
+            saldoLabel.Text = ((Application.Current as App).Saldo).ToString(formatSaldo);
+        }
+
+        void NastavTextSaldoCelkem()
+        {
+            string formatSaldo = (saldoCelkem < TimeSpan.Zero ? "\\-" : "") + "hh\\:mm\\:ss";
+            saldoCelkemLabel.Text = ((Application.Current as App).SaldoCelkem).ToString(formatSaldo);
+        }
+
+        void NastavTexty()
+        {
+            NastavTextPrichod();
+            NastavTextOdchod();
+            NastavTextSaldo();
+            NastavTextSaldoCelkem();
+        }
+
+    }
+}
